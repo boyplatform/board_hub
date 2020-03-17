facfb5b3659a8961c11734fd6592e7bcbe334ab1
boybian <boybian@126.com>
2020-03-17 16:13:31 +0800

    support Lan&internet network mode.

--- boyHub/webApi/boyHubEntry.js
+++ boyHub/webApi/boyHubEntry.js
@@ -25,7 +25,10 @@ app.use(bodyParser.urlencoded({extended:false}));
 
 //Programe Entry
 var server=app.listen(8080,'0.0.0.0',function(){
- 
+      //internet模式下启动时初始化并缓存本机公网IP
+      if(conf.platformArch.crystalCluster.CrystalClusterNetworkMode==="internet"){
+         boyHubCommon.initCurrentServerPubIpAdress();
+      }
      console.log('Intelligent boy-hub is running on current crystal node at:'+(new Date()).toLocaleString()," on IP:",boyHubCommon.getCurrentServerIpAdress().trim());
      memoryNodeCache.setConn({});
       
